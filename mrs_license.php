3dd14d04b344ec0239a92844ae5ec524ae973fe9
sultansofsweat <41843365+sultansofsweat@users.noreply.github.com>
2026-07-22 01:28:18 -0400

    Move license.php to mrs_license.php
    Change due to the way github processes license files.

---
+++ mrs_license.php
@@ -0,0 +1,26 @@
+<?php
+	require("errorhandler.php");
+	
+	$database_folder=".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "mrs" . DIRECTORY_SEPARATOR;
+	
+	session_start();
+	
+	$admin=false;
+	if(!empty($_SESSION['mrsadmin']) && $_SESSION['mrsadmin'] == dirname(__FILE__))
+	{
+		$admin=true;
+	}
+?>
+<html>
+<head>
+<title>Music Request System LITE - License</title>
+<link rel="icon" href="favicon.ico">
+<link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+<h1>Music Request System LITE</h1>
+<h2>Software License</h2>
+<p style="font-family:'Courier New',monospace;"><?php echo nl2br(file_get_contents("LICENSE.md")); ?></p>
+<p><a href="about.php">Abscond</a></p>
+</body>
+</html>
